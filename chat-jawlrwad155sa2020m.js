0648793ac8c4fa0ca010573bc97fe3c8cd1f6a57
ita7a <57022144+ita7a@users.noreply.github.com>
2019-10-27 02:36:56 +0300

    Create chat-jawlrwad155sa2020m.js

---
+++ chat-jawlrwad155sa2020m.js
@@ -0,0 +1,44 @@
+$('.checkbox label').html(`<input id="stealth" type="checkbox" value=""><img id="as13" src="" width="70%" style="">`)
+
+$( "a.label.label-primary.fl > img.fl" ).replaceWith('<img src="/sico/1572132787207.gif" class="fl" style="margin-Right: 2px; width: 70px;">');
+
+$(`<img src="/sico/1571434199503.gif" class="fr" style="width:100%;margin-top: 1px;">
+<a href="#" style="border-radius: 15px 0px;width:110px;margin:3px;" class="fl mini  fa fa-star-half-o btn btn-default">⠀الإشتراكات⠀</a>
+<a href="#" style="border-radius: 15px 0px;width:110px;margin:3px;" class="fl mini fa fa-bell btn btn-default">⠀القوانيـن⠀</a>  
+<a href="#" style="border-radius: 15px 0px;width:110px;margin:3px;" class="fl mini  fa fa-heartbeat btn btn-default">⠀التطبيـق⠀</a>  
+<div style="border-radius: 2px 9px 9px 9px;width:100%;margin:2px;border: 2px solid #8F0000;"  id="asim33" class="btn btn-primary btn-sm" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"> ادارة الموقع</div><div class="collapse" id="collapseExample"><center><div class="well">
+<b>
+<font color="#78939C"> اداره  الموقع </font> </BR>
+</BR>  MuHanad T7 </BR>
+ </BR> للشكاوي والاستفسارات </BR>
+<font color="#BF1D6E">Line:mudx999~Snap:muh.911</font></BR>     
+`).insertBefore('.nav-tabs');
+$("<a href=' ' target='_blank' class='fr btn btn-info fa fa-heart'>..رواد الخليج..</a>").insertAfter('.loginstat');
+$(".label-primary").css("border-radius","24px 24px 24px 24px")
+$(`<link href="https://fonts.googleapis.com/css?family=Changa" rel="stylesheet">
+<marquee direction="right" width="65%" direction="right" style="margin-bottom: -7px;font-family: 'Changa', sans-serif; height: 20px;color:#FFF7AB;" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5">🌸مرحبا بكم نتمنى لكم قضاء أجمل الاوقات
+</marquee></font></b></td>
+`).insertBefore('span.s1.fa.fa-user.label.badgex.label-as-badge.label-success');
+
+$("div#tlogins button.btn.btn-primary").click(function(){var myVar = setInterval(function(){ var usmsgw = $(".pmsgc").length;if(usmsgw > 0){
+
+$(`<div class="uzr fl corner borderg mm" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:#F7E7E7;">
+<img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url;" class="fl fitimg hand u-pic ">
+<div class="uzr fl" style="padding:0px;width:80%">
+<div style="padding:0px;width:100%;" class="fl">
+<img class="fl u-ico" alt="" src="/sico/1569974559772.gif">
+<span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: rgb(199, 103, 48);" class="corner nosel u-topic dots fl hand">‎ اداره شات رواد الخليـج </span>
+</div>
+<br>
+<div style="padding: 0px; width: 100%; color: rgb(60, 0, 255);" class=" u-msg break fl">
+<div style="padding: 0px;width: 100%;color: rgb(60, 0, 255);text-align: left;display: block;margin-top: -17px;" class=" u-msg break fl"> مرحباً بك عزيزي الزآئر 👋 <h1 style="display: inline-block;color: red;">`+ getuser(myid).topic+ `</h1> نقدّر تواجدك معنا، ونتمنى لك يوماً سعيداً إن شاء الله </div>
+</div>
+</div></div>
+`).appendTo('div#d2')
+clearInterval(myVar);
+}else{console.log(usmsgw)} }, 2000);})
+
+$(`<center><div><div width="99.5%" style="background-image:url(/sico/z1dm3io75710.jpg);color: #000000;border-radius: 5px 5px 5px5px;border-bottom: 2px solid #ABABAB;border-top: 2px solid #ABABAB;padding-bottom: 2px!important;" ><font style="background-color: #BCC6CF;border-radius: 15px 15px 15px 15px;font-family: 'jazeera',FontAwesome;padding: 2px 10px 2px 10px;margin: 7px 10px 7px 10px;" >مرحبآ بكم في شات رواد الخليج </font></div></div><center>`).insertBefore('#d2');$('.nav-tabs').addClass('fl').css('width','100%');$
+('a[href="/sico/1572095805304.jpg"]').removeAttr('href');
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera"><link rel="stylesheet" href="">
+</style>`).insertBefore('span.fl.fa.fa-send');
